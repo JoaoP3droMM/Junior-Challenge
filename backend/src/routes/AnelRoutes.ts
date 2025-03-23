0f49b8498111bb33ae99714c87494a0d4bacd9b2
JoaoPedroLouvre <105229253+JoaoP3droMM@users.noreply.github.com>
2025-03-23 17:39:44 -0300

    Corrigindo método PUT

--- backend/src/routes/AnelRoutes.ts
+++ backend/src/routes/AnelRoutes.ts
@@ -5,7 +5,7 @@ const router = Router()
 
 router.post('/aneis', AnelController.criar)
 router.get('/aneis', AnelController.listar)
-router.put('/aneis', AnelController.atualizar)
+router.put('/aneis/:id', AnelController.atualizar)
 router.delete('/aneis/:id', AnelController.deletar)
 
 export default router
